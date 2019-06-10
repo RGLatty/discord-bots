8e26afca0f75dc7c4ae256687929ff7cb89bc3e9
RGLatty <43281044+RGLatty@users.noreply.github.com>
2019-06-09 20:39:44 -0400

    Create poll.js

---
+++ commands/poll.js
@@ -0,0 +1,32 @@
+    
+// This command requires the discord-js package to create embeds
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args, tools) => {
+
+    // Role Verification (Optional)
+    if (!message.member.roles.find(r => r.name === 'Manager', 'Owner')) return message.channel.send('This requires the role: roleName');
+
+    // Permission Verification (Optional)
+    if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('This requires the permission: ADMINISTRATOR');
+
+    if (!args[0]) return message.channel.send('Proper Usage: !poll <question>');
+
+    const embed = new Discord.RichEmbed()
+        .setColor("BLUE")
+        .setFooter(`Poll created by ${message.author.username}`)
+        .setDescription(args.join(' '))
+        .setTitle('Poll');
+
+    let everyone = message.channel.send("@everyone");
+    let msg = await message.channel.send(embed);
+
+    await msg.react('✅'); 
+    await msg.react('❌');
+
+    message.delete({timeout: 1000}); // This waits 1000 milliseconds before deleting (1 second)
+
+} 
+module.exports.help = {
+  name: "poll"
+}

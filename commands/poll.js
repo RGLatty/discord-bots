cc289c07dd997af581f9338894113cc068bf6f03
RGLatty <43281044+RGLatty@users.noreply.github.com>
2019-06-09 20:53:45 -0400

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,15 +1,9 @@
-    
-// This command requires the discord-js package to create embeds
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args, tools) => {
 
-    // Role Verification (Optional)
     if (!message.member.roles.find(r => r.name === 'Manager', 'Owner')) return message.channel.send('This requires the role: roleName');
-
-    // Permission Verification (Optional)
     if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('This requires the permission: ADMINISTRATOR');
-
     if (!args[0]) return message.channel.send('Proper Usage: !poll <question>');
 
     const embed = new Discord.RichEmbed()
